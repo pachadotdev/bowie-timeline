bdd74e7cb5087e53e8b224975ef25968beb89eb6
Mauricio Vargas <mvargas@dcc.uchile.cl>
2017-11-15 16:46:06 -0300

    updated code and styles

--- timeline.R
+++ timeline.R
@@ -1,25 +1,28 @@
-setwd("/Users/pacha/bowie-timeline/")
-library(plyr)
-library(RCurl)
-library(rCharts)
+if (!require("pacman")) install.packages("pacman")
+p_load(plyr,dplyr,readr,RCurl)
+p_load_gh("ramnathv/rCharts")
 
-discography <- read.csv("discography.csv", as.is = TRUE)
-discography <- alply(discography, 1, function(x) {
-  list(startDate = x$Start.Date, headline = x$Headline, text = x$Text, asset = list(media = x$Media))
+
+discography = read_csv("discography.csv", col_types = cols(.default = "c")) 
+
+discography = alply(discography, 1, function(x) {
+  list(startDate = x$`Start Date`, headline = x$Headline, text = x$Text, asset = list(media = x$Media))
 })
 
 tl = Timeline$new()
 tl$main(headline = paste0("David Bowie"), type = "default", text = paste0("Discography ", 
         format(as.Date("1967-01-01"), "%Y"), "-", format(as.Date("2016-01-01"), "%Y")), 
         startDate = format(as.Date("1967-01-01"), "%Y"), asset = list(media = "img/cover.jpg"))
-names(discography) <- NULL
+names(discography) = NULL
 tl$event(discography)
 
 tl$save("index.html")
 
 # Modify JS Path
-js <- paste(readLines("index.html", warn = F), collapse = "\n")
-js <- gsub("/Library/Frameworks/R.framework/Versions/3.2/Resources/library/rCharts/libraries/timeline/js/storyjs-embed.js", 
-           "timeline/js/storyjs-embed.js", 
-           js)
-writeLines(js, con = "index.html")
+js = paste(read_lines("index.html"), collapse = "\n") %>% 
+  gsub("/home/pacha/GitHub/bowie-timeline/", "", .) %>% 
+  gsub("1/1/", "19", .) %>% 
+  gsub(" 00:00", "", .) %>% 
+  gsub("190", "200", .) %>% 
+  gsub("191", "201", .)
+write_lines(js, "index.html")
